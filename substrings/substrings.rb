b2911049ef70da32c0ed2b1f7505155b4ac05d80
Adam Smith <mailformrsmith@gmail.com>
2024-10-31 22:58:20 -0400

    Uploading ruby-substrings project

---
+++ substrings/substrings.rb
@@ -0,0 +1,13 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(find, dictionary)
+    list=Hash.new
+    dictionary.each do |word|
+        if find.downcase.scan(word).count > 0
+            list[word] = find.downcase.scan(word).count
+        end
+    end
+    return list
+end
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
